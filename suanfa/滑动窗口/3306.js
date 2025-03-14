d8571c3e97750b974183675f260d7570c50c6cef
Philia8 <philia_liu@qq.com>
2025-03-14 10:41:36 +0800

    window

--- suanfa/滑动窗口/3306.js
+++ suanfa/滑动窗口/3306.js
@@ -9,32 +9,37 @@ var countOfSubstrings = function (word, k) {
   let map = new Map();
   let consonant = 0;
   let start = 0;
+  let sum = 0;
 
   // 辅音字母 !VOWEL.inclueds(ch);
   for (let i = 0; i < word.length; i++) {
-    const curCh = word.charAt(i);
-    if (!VOWEL.includes(curCh)) consonant++;
-    else map.set(curCh, (map.get(curCh) || 0) + 1);
-    if (consonant === k && map.size === 5) res++;
+    const ch = word.charAt(i);
+    if (!VOWEL.includes(ch)) consonant++;
     else {
-      // 滑动窗口可以缩小的条件
-      while (true) {
-        const char = word.charAt(start);
-        if (VOWEL.includes(char)) {
-          start++;
-          map.set(char, map.get(char) - 1);
-          if (!map.get(char)) map.delete(char);
-        } else if (consonant > k) {
-          consonant--;
-          start++;
-        }
-        if (consonant === k && map.size === 5) res++;
-        else if (consonant < k || map.size < 5) break;
+      map.set(ch, (map.get(ch) || 0) + 1);
+      sum++;
+    }
+    if (k === consonant && map.size === 5) res++;
+
+    while (consonant >= k || sum > 5) { // 窗口缩小条件：辅音字母个数 > k || 元音字母个数可减少
+      const s_ch = word.charAt(start);
+      if (!VOWEL.includes(s_ch)) {
+        consonant--;
+        start++;
       }
+      else {
+        map.set(s_ch, map.get(s_ch) - 1);
+        if (!map.get(s_ch)) map.delete(s_ch);
+        start++;
+        sum--;
+      }
+      if (k === consonant && map.size === 5) res++;
     }
   }
   return res;
 };
 
-const word = "iqeaouqi", k = 2;
+// const word = "iqeaouqi", k = 2;
+// const word = "ieaouqqieaouqq", k = 1;
+const word = "eiaaoui", k = 0;
 console.log(countOfSubstrings(word, k));
