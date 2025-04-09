a2fc987a11090e8248c875910fc2dfd9f742fa00
Philia8 <philia_liu@qq.com>
2025-04-09 16:54:22 +0800

    vue

---
+++ vue/apps/npm-create-pro/src/main.js
@@ -0,0 +1,6 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue' //根组件
+
+createApp(App).mount('#app') // 应用挂载在容器#app上
---
+++ vue/basic/module/my-component.js
@@ -0,0 +1,6 @@
+export default {
+  data() {
+    return { count: 18 };
+  },
+  template: `<div>Count is: {{ count }}</div>`
+};
