73e8e71627790dd29ef1b6764aa24ad4e76c4bc9
Philia8 <philia_liu@qq.com>
2025-04-26 15:56:51 +0800

    vue

--- vue/apps/cli-pro/src/App.vue
+++ vue/apps/cli-pro/src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <BlogPost title="hello"></BlogPost>
+  <Counter></Counter>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script type="module">
+import BlogPost from './components/BlogPost';
+import Counter from './components/Counter.js';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BlogPost,
+    Counter
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ vue/apps/cli-pro/src/components/BlogPost.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>{{title}}</div>
+</template>
+
+<script>
+  export default {
+    name: 'BlogPost',
+    props: ["title"]
+  }
+</script>
+
+<style scoped>
+
+</style>
